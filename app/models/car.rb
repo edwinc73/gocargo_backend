8828989c0e83406e0da015695844d1ac6284c44a
fanxin li <lexxshinjj@gmail.com>
2023-06-17 17:33:06 +0800

    edit-models

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,8 +2,5 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :favourite_cars
-  validates :car_model, presence: true
-  validates :mileage, presence: true
-  validates :city, presence: true
-  validates :price_per_day, presence: true
+  validates :car_model, :mileage, :city, :price_per_day, presence: true
 end
