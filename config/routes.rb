314277efdeaa40922990c21c995bba4eb57b943b
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-17 16:57:55 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "users/profile", to: "users#profile"
+  resources :cars, only: %i[show create index] do
+    post "bookings", to: "bookings#create"
+  end
+  resources :bookings, only: %i[show update]
 end
