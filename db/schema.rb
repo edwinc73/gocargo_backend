c18820b2b0c206361477e9f000a33f4d006607da
fanxin li <lexxshinjj@gmail.com>
2023-06-18 13:34:13 +0800

    create favourite car seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_015159) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_051455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_18_015159) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "mileage"
+    t.integer "mileage"
     t.index ["user_id"], name: "index_cars_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,9 +16,10 @@ end
 
 100.times do
   car_brand = Faker::Vehicle.make
-  car_model = Faker::Vehicle.model(make_of_model: car_brand)
-  car_image = "https://cdn.imagin.studio/getimage?customer=img&make=#{car_brand}&modelFamily=#{car_model}&angle=1&paintId=2t"
-  mileage = rand(1.0..100000.0).round(1)
+  model = Faker::Vehicle.model(make_of_model: car_brand)
+  car_model = "#{car_brand} #{model}"
+  car_image = "https://cdn.imagin.studio/getimage?customer=img&make=#{car_brand}&modelFamily=#{model}&angle=1&paintId=2t"
+  mileage = rand(1..100000)
   city = ["Beijing", "Shanghai", "Shenzhen", "Xian", "Guangzhou", "Chongiqng", "Chengdu", "Hangzhou", "Nanjing"].sample
   price_per_day = rand(200..10000).round(2)
   user_id = rand(1..30)
@@ -30,4 +31,8 @@ end
     price_per_day: price_per_day,
     user_id: user_id
   )
+end
+
+50.times do
+  FavouriteCar.create!(user_id: rand(1..30), car_id: rand(1..100))
 end
