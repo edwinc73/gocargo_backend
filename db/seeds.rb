3476f8767c27c3b16b9c0f8505f1ad87b846e1ab
fanxin li <lexxshinjj@gmail.com>
2023-06-18 14:22:29 +0800

    booking seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ end
   car_image = "https://cdn.imagin.studio/getimage?customer=img&make=#{car_brand}&modelFamily=#{model}&angle=1&paintId=2t"
   mileage = rand(1..100000)
   city = ["Beijing", "Shanghai", "Shenzhen", "Xian", "Guangzhou", "Chongiqng", "Chengdu", "Hangzhou", "Nanjing"].sample
-  price_per_day = rand(200..10000).round(2)
+  price_per_day = rand(200..3000).round(2)
   user_id = rand(1..30)
   Car.create!(
     car_model: car_model,
@@ -35,4 +35,22 @@ end
 
 50.times do
   FavouriteCar.create!(user_id: rand(1..30), car_id: rand(1..100))
+end
+
+50.times do
+  start_date = Faker::Time.between(from: DateTime.now, to: '2023-07-06')
+  return_date = Faker::Time.between(from: start_date, to: '2023-07-06')
+  user_id = rand(1..30)
+  car_id = rand(1..100)
+  price_per_day = Car.find(car_id).price_per_day
+  total_price = ((return_date - start_date)/1.day).ceil * price_per_day
+  Booking.create!(
+    start_date: start_date,
+    return_date: return_date,
+    user_id: user_id,
+    car_id: car_id,
+    total_price: total_price,
+    completed: false,
+    approved: false
+  )
 end
