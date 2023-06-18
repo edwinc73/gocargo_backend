d267ee01905c03455953a67464134f13e8d8a26b
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-18 12:39:53 +0800

    added booking to profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,17 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def profile
     @user = User.find(1) #need to update this code to the other users.
+
     @favourite_cars = []
     favourite_cars_list = @user.favourite_cars
     favourite_cars_list.each do |favourite_car|
       car_id = favourite_car.car_id
       @favourite_cars << Car.find(car_id)
+    end
+
+    @bookings = @user.bookings.map do |booking|
+      booking.car = Car.find(booking.car_id)
+      booking
     end
   end
 end
