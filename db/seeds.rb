9aaf4764f7841c4493371d3277c2630b9ca917ef
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-18 18:21:42 +0800

    made it so that seed will always work even if the id doesnt start at 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,48 @@
+FavouriteCar.destroy_all
+Booking.destroy_all
+Car.destroy_all
+User.destroy_all
+
+30.times do
+  username = Faker::Internet.username(specifier: 5..18)
+  email = Faker::Internet.email
+  avatar = ['dogs', 'sports', 'cars'].sample
+  avatar_image = Faker::LoremFlickr.image(size: "100x100", search_terms: [avatar])
+  User.create!(username: username, email: email, avatar_image: avatar_image)
+end
+
+100.times do
+  car_brand = Faker::Vehicle.make
+  model = Faker::Vehicle.model(make_of_model: car_brand)
+  car_model = "#{car_brand} #{model}"
+  car_image = "https://cdn.imagin.studio/getimage?customer=img&make=#{car_brand}&modelFamily=#{model}&angle=1&paintId=2t"
+  mileage = rand(1..100000)
+  city = ["Beijing", "Shanghai", "Shenzhen", "Xian", "Guangzhou", "Chongiqng", "Chengdu", "Hangzhou", "Nanjing"].sample
+  price_per_day = rand(200..3000).round(2)
+  Car.create!(
+    car_model: car_model,
+    car_image: car_image,
+    mileage: mileage,
+    city: city,
+    price_per_day: price_per_day,
+    user: User.all.sample
+  )
+end
+
 50.times do
-  FavouriteCar.create!(user_id: rand(1..30), car_id: rand(1..100))
+  FavouriteCar.create!(user: User.all.sample, car: Car.all.sample)
 end
 
 50.times do
   start_date = Faker::Time.between(from: DateTime.now, to: '2023-07-06')
   return_date = Faker::Time.between(from: start_date, to: '2023-07-06')
-  user_id = rand(1..30)
-  car_id = rand(1..100)
-  price_per_day = Car.find(car_id).price_per_day
+  price_per_day = Car.all.sample.price_per_day
   total_price = ((return_date - start_date) / 1.day).ceil * price_per_day
   Booking.create!(
     start_date: start_date,
     return_date: return_date,
-    user_id: user_id,
-    car_id: car_id,
+    user: User.all.sample,
+    car: Car.all.sample,
     total_price: total_price,
     completed: false,
     approved: false
