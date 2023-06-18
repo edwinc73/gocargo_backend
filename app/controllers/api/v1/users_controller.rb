5059d48eb3c3d287de3bc6283285442cbc2a3140
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-18 12:08:01 +0800

    added comment on users#profile

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def profile
-    @user = User.find(1)
+    @user = User.find(1) #need to update this code to the other users.
     @favourite_cars = []
     favourite_cars_list = @user.favourite_cars
     favourite_cars_list.each do |favourite_car|
