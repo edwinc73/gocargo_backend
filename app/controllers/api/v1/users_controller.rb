5fe0d64abb625e71f7c253c314c060f04c96dc10
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-18 12:04:36 +0800

    added profile jbuilder, now able to access favourite car info and user info using api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,15 @@
 class Api::V1::UsersController < Api::V1::BaseController
   def profile
     @user = User.find(1)
-    #find favourite cars
-    #see my profile
-    # in profile we can see the bookings list, my cars and
+    @favourite_cars = []
+    favourite_cars_list = @user.favourite_cars
+    favourite_cars_list.each do |favourite_car|
+      car_id = favourite_car.car_id
+      @favourite_cars << Car.find(car_id)
+    end
   end
 end
+
+#find favourite cars
+#see my profile
+# in profile we can see the bookings list, my cars and
--- app/views/api/v1/users/profile.json.jbuilder
+++ app/views/api/v1/users/profile.json.jbuilder
@@ -1,1 +1,6 @@
-json.extract! @user, :username, :avatar_image, :rating, :email, :favourite_cars
+json.extract! @user, :username, :avatar_image
+json.favourite_cars do
+  json.array! @favourite_cars do |car|
+    json.extract! car, :car_model, :mileage, :car_image, :city, :price_per_day, :rating
+  end
+end
