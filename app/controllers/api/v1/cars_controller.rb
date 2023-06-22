fb98d26f3c1ebf0aff86b87070ae5624e0d99a19
fanxin li <lexxshinjj@gmail.com>
2023-06-23 07:36:45 +0800

    debug cars controller, add rating in seed

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,19 +1,18 @@
 class Api::V1::CarsController < Api::V1::BaseController
+  before_action :set_car, only: [ :show ]
   def index
     @cars = Car.all
   end
 
   def show
-    @car = Car.find(params[:id])
     @owner = User.find(@car.user_id)
   end
 
   def create
     @car = Car.new(car_params)
-    #get user id
-    #@car.user = user
+    @car.user = User.last
     if @car.save
-      render :show, status: :created
+      redirect_to api_v1_car_path(@car), status: 303
     else
       render_error
     end
@@ -23,5 +22,9 @@ class Api::V1::CarsController < Api::V1::BaseController
 
   def car_params
     params.require(:car).permit(:car_model, :mileage, :car_image, :city, :price_per_day)
+  end
+
+  def set_car
+    @car = Car.find(params[:id])
   end
 end
