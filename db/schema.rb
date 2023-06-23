4e210a4dd4b1d9a89f27693247defd60080a1171
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-24 03:06:11 +0800

    accept prev commit in schema

--- db/schema.rb
+++ db/schema.rb
@@ -54,10 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_23_170332) do
     t.bigint "car_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
-    t.boolean "canceled"
-=======
->>>>>>> master
     t.integer "days"
     t.boolean "cancelled"
     t.index ["car_id"], name: "index_bookings_on_car_id"
