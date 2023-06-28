c55649ee89f857e631bb4a739422f22a1f59238f
edwinc73 <92707696+edwinc73@users.noreply.github.com>
2023-06-28 22:45:32 +0800

    Update car.rb

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,6 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :favourite_cars
+  gem "dotenv-rails", groups: [:development, :test]
   validates :car_model, :car_brand, :mileage, :city, :price_per_day, presence: true
 end
