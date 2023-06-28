65b034a5e71b1e62bcff9ff073c5d12f42e3b7cf
edwinc73 <92707696+edwinc73@users.noreply.github.com>
2023-06-28 16:01:07 +0800

    Revert "added description for the premium cars and also add one more brand ma…"

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,60 +18,46 @@ end
 ferrari = {
   car_brand: "Ferrari",
   model: "spider 348",
-  car_image: "https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  description: "Rent a stunning Ferrari Spider 348 with a sleek design and top-of-the-line features. Don't miss out on this rare opportunity."
+  car_image: "https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 }
 
 porsche = {
   car_brand: "Porsche",
   model: "911 GT",
-  car_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Porsche_911_992_GT3.jpg/1200px-Porsche_911_992_GT3.jpg",
-  description: "Experience the thrill of driving a legendary sports car with a used Porsche 911 GT. With its powerful engine and agile handling, this car is perfect for a weekend getaway or a special event."
+  car_image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Porsche_911_992_GT3.jpg/1200px-Porsche_911_992_GT3.jpg"
 }
 
 koenigsegg = {
   car_brand: "Koenigsegg",
   model: "Gemera GT",
-  car_image: "https://www.motortrend.com/uploads/sites/11/2020/03/Koenigsegg-Gemera-in-motion-3.jpg?fit=around%7C875:492",
-  description: "Rent the Koenigsegg Gemera GT for an unforgettable driving experience. This used sports car features a powerful engine and state-of-the-art technology, perfect for a luxurious weekend getaway."
+  car_image: "https://www.motortrend.com/uploads/sites/11/2020/03/Koenigsegg-Gemera-in-motion-3.jpg?fit=around%7C875:492"
 }
 
 aston_martin = {
   car_brand: "Aston Martin",
   model: "Valkyrie 2015",
-  car_image: "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/aston-martin-valkyrie-2023-36_0.jpg?itok=BeIgNQix",
-  description: "Sleek and powerful Aston Martin for rent - a stylish choice for discerning drivers. Experience the James Bond connection with this iconic and luxurious ride."
+  car_image: "https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/aston-martin-valkyrie-2023-36_0.jpg?itok=BeIgNQix"
 }
 
 mclaren = {
   car_brand: "Mclaren",
   model: "Senna #434",
-  car_image: "https://bringatrailer.com/wp-content/uploads/2023/02/2019_mclaren_senna_img_7358-6-64343.jpg?fit=940%2C627",
-  description: "Experience the unmatched performance of my McLaren. This hypercar combines exquisite design with exhilarating speed, creating the ultimate driving experience for enthusiasts."
+  car_image: "https://bringatrailer.com/wp-content/uploads/2023/02/2019_mclaren_senna_img_7358-6-64343.jpg?fit=940%2C627"
 }
 
 lamborghini = {
   car_brand: "Lamborghini",
   model: "Huracan LP570",
-  car_image: "https://images.pexels.com/photos/10394779/pexels-photo-10394779.jpeg",
-  description: "Experience the sheer thrill and unmatched performance of this iconic supercar. Its captivating design and exhilarating power make it the ultimate choice for discerning drivers."
+  car_image: "https://images.pexels.com/photos/10394779/pexels-photo-10394779.jpeg"
 }
 
 bugatti = {
   car_brand: "Bugatti",
   model: "Bolide",
-  car_image: "https://hips.hearstapps.com/hmg-prod/images/bugatti-bolide-106-1628792006.jpg?crop=0.922xw:1.00xh;0.0450xw,0&resize=980:*",
-  description: "Discover the epitome of speed and excitement with my Bugatti Bolide. This extraordinary vehicle pushes the boundaries of performance, delivering breathtaking power and an unforgettable driving experience."
+  car_image: "https://hips.hearstapps.com/hmg-prod/images/bugatti-bolide-106-1628792006.jpg?crop=0.922xw:1.00xh;0.0450xw,0&resize=980:*"
 }
 
-maserati = {
-  car_brand: "Maserati",
-  model: "MC20",
-  car_image: "https://news.italy24.press/content/uploads/2023/05/31/c6d161ec51.jpg",
-  description: "As an owner of the Maserati MC20, I can confidently say that this car is a masterpiece of engineering and design. Its advanced technology and pure power create an unrivaled driving experience that you won't find anywhere else."
-}
-
-premium_car = [ferrari, porsche, mclaren, aston_martin, koenigsegg, lamborghini, bugatti, maserati]
+premium_car = [ferrari, porsche, mclaren, aston_martin, koenigsegg, lamborghini, bugatti]
 
 100.times do
   car = premium_car.sample
