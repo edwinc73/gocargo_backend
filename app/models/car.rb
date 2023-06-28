22e7f351660b59225abcefd3d89066a35b62b249
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-28 11:35:36 +0800

    added car_brand to show.json, cars and bookings

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,5 +2,5 @@ class Car < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :favourite_cars
-  validates :car_model, :mileage, :city, :price_per_day, presence: true
+  validates :car_model, :car_brand, :mileage, :city, :price_per_day, presence: true
 end
