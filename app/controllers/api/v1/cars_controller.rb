ed8c26024618e72de7e89abd9c6bc2fb80793109
Edwin Cheng <edwincheng09@googlemail.com>
2023-06-29 18:48:22 +0800

    getting active storage to wowrk

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CarsController < Api::V1::BaseController
-  before_action :set_car, only: [ :show ]
+  before_action :set_car, only: [ :show]
   def index
     @cars = Car.all
   end
@@ -10,7 +10,7 @@ class Api::V1::CarsController < Api::V1::BaseController
 
   def create
     @car = Car.new(car_params)
-    @car.user = User.last
+    @car.user = @current_user
     if @car.save
       redirect_to api_v1_car_path(@car), status: 303
     else
@@ -18,7 +18,22 @@ class Api::V1::CarsController < Api::V1::BaseController
     end
   end
 
+  def upload_img
+    @car = Car.last
+    photo = params[:photos]
+    @car.photos.attach(io:photo.tempfile, filename:photo.original_filename)
+    @car.save!
+  end
+
   private
+
+  # def img_params
+  #   params.require(:car).permit(:photos)
+  # end
+
+  # def car_params
+  #   params.require(:photod).permit(:car_model, :mileage, :car_image, :city, :price_per_day, :car_brand, :photos, photos: [])
+  # end
 
   def car_params
     params.require(:car).permit(:car_model, :mileage, :car_image, :city, :price_per_day, :car_brand, :photos)
--- app/views/api/v1/cars/show.json.jbuilder
+++ app/views/api/v1/cars/show.json.jbuilder
@@ -1,6 +1,11 @@
 json.car do
   json.extract! @car, :car_model,:car_brand,:mileage, :car_image, :city, :price_per_day, :rating, :id
 end
+json.photos do
+  json.array! @car.photos do |photo|
+    json.extract! photo.blob, :key, :filename
+  end
+end
 
 json.owner do
   json.extract! @owner, :username, :avatar_image, :rating, :id
